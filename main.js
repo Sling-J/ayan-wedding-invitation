be6fac56f276b0d71f5b2818d2db27e11bce032c
Nurel <nurel32@gmail.com>
2023-05-09 05:10:23 +0600

    feat: add countdown

--- main.js
+++ main.js
@@ -27,4 +27,51 @@ window.addEventListener("load", function () {
         submitButton.disabled = false
       })
   });
+
+  const countDownDate = new Date('May 9, 2024 05:10:00').getTime();
+  const dayEl = document.getElementById('js-days')
+  const hoursEl = document.getElementById('js-hours')
+  const minutesEl = document.getElementById('js-minutes')
+  const secondsEl = document.getElementById('js-seconds')
+
+  const daysContainer = document.querySelector('.days')
+  const startedText = document.getElementById('started')
+
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  function getDate(distance) {
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return {
+      days, hours, minutes, seconds
+    }
+  }
+
+  const {days, hours, minutes, seconds} = getDate(distance)
+
+  dayEl.innerHTML = days < 0 ? 0 : days
+  hoursEl.innerHTML = hours < 0 ? 0 : hours
+  minutesEl.innerHTML = minutes < 0 ? 0 : minutes
+  secondsEl.innerHTML = seconds < 0 ? 0 : seconds
+
+  const x = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const {days, hours, minutes, seconds} = getDate(distance)
+
+    dayEl.innerHTML = days < 0 ? 0 : days
+    hoursEl.innerHTML = hours < 0 ? 0 : hours
+    minutesEl.innerHTML = minutes < 0 ? 0 : minutes
+    secondsEl.innerHTML = seconds < 0 ? 0 : seconds
+
+    if (distance < 1) {
+      clearInterval(x);
+      daysContainer.classList.add('hide')
+      startedText.classList.remove('hide')
+    }
+  }, 1000);
 });
